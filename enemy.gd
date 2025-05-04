5e2a95481483c65714ec8c0ce0a3df7370d0dcaf
Raffaele Lauro <raff.lauro@gmail.com>
2025-05-04 15:39:45 +0200

    Fixed player and enemy collision + improved parry effect

--- enemy.gd
+++ enemy.gd
@@ -2,21 +2,20 @@ extends RigidBody3D
 
 class_name Enemy
 
-@export var speed: float = 10.0
+@export var speed: float = 1000.0
 @export var target: Node3D
+@export var repulsion_magnitude: float = 2000
+@export var repulsion_torque: float = 1000
 var stop: bool = false
 	
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if !stop and target != null and !position.is_equal_approx(target.position):
 		look_at(target.position)
-		position = position.move_toward(target.position, delta*speed)
+		apply_force((target.position-position).normalized() * delta * speed)
 	
 func is_parried() -> void:
 	print('PARRIED!')
 	stop = true
-	rotation.x = PI/2
 	
-	apply_force((position - target.position).normalized()*1000)
-
-func _on_body_entered(body:Node) -> void:
-	print("EXPLOSION")
+	apply_force((position - target.position).normalized()*repulsion_magnitude)
+	apply_torque(Vector3(1, 0, 0) * repulsion_torque)
